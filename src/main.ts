b7dc6bf66d0f6487f9ecc3eb74fa43bb0b6e2ff0
Arnold Dadayan <adadayan@servicetitan.com>
2024-12-10 16:29:35 +0400

    update configs

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
+  app.enableCors();
   await app.listen(3000);
   console.log('Application is running on http://localhost:3000/api');
 }
