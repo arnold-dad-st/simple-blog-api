b5a5462fa212febd5a3ba1c5091babbfdd27cf23
Arnold Dadayan <adadayan@servicetitan.com>
2024-12-10 16:54:23 +0400

    swagger static

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostsModule } from './posts/posts.module';
+import { join } from 'path';
 
 @Module({
-  imports: [PostsModule],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'swagger-static'),
+      serveRoot: process.env.NODE_ENV === 'development' ? '/' : '/swagger',
+    }),
+    PostsModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
