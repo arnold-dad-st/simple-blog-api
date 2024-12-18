7d60d923f5c741ea64f7b2ba28ed6e234ee3719d
Arnold Dadayan <adadayan@servicetitan.com>
2024-12-18 12:09:34 +0400

    update

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -2,38 +2,38 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { Post } from './post.entity';
 import { v4 as uuid } from 'uuid';
 
+const posts: Post[] = [];
+
 @Injectable()
 export class PostsService {
-  private posts: Post[] = [];
-
   create(post: Partial<Post>): Post {
     const newPost = { id: uuid(), ...post } as Post;
-    this.posts.push(newPost);
+    posts.push(newPost);
     return newPost;
   }
 
   findAll(): Post[] {
-    return this.posts;
+    return posts;
   }
 
   findOne(id: string): Post {
-    const post = this.posts.find((p) => p.id === id);
+    const post = posts.find((p) => p.id === id);
     if (!post) throw new NotFoundException(`Post with ID "${id}" not found.`);
     return post;
   }
 
   update(id: string, updatePostDto: Partial<Post>): Post {
-    const postIndex = this.posts.findIndex((p) => p.id === id);
+    const postIndex = posts.findIndex((p) => p.id === id);
     if (postIndex === -1)
       throw new NotFoundException(`Post with ID "${id}" not found.`);
-    this.posts[postIndex] = { ...this.posts[postIndex], ...updatePostDto };
-    return this.posts[postIndex];
+    posts[postIndex] = { ...posts[postIndex], ...updatePostDto };
+    return posts[postIndex];
   }
 
   delete(id: string): void {
-    const postIndex = this.posts.findIndex((p) => p.id === id);
+    const postIndex = posts.findIndex((p) => p.id === id);
     if (postIndex === -1)
       throw new NotFoundException(`Post with ID "${id}" not found.`);
-    this.posts.splice(postIndex, 1);
+    posts.splice(postIndex, 1);
   }
 }
