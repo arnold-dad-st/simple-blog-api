e4d491c9ae7d75320caae3c6f90401dcd8cf8716
Arnold Dadayan <adadayan@servicetitan.com>
2024-12-18 16:01:49 +0400

    disable cors

--- src/main.ts
+++ src/main.ts
@@ -27,8 +27,6 @@ async function bootstrap() {
   app.enableCors({
     origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
   });
   await app.listen(3000);
   console.log('Application is running on http://localhost:3000/api');
