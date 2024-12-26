c6bd9a0cbec30da34aeedc31b8cbe31a9dc09cc2
Arnold Dadayan <adadayan@servicetitan.com>
2024-12-26 17:16:54 +0400

    add env key

--- src/file-upload/file-upload.service.ts
+++ src/file-upload/file-upload.service.ts
@@ -1,13 +1,17 @@
 import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { SupabaseService } from '../supabase/supabase.service';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class FileUploadService {
-  constructor(private readonly supabaseService: SupabaseService) {}
+  constructor(
+    private readonly supabaseService: SupabaseService,
+    private readonly configService: ConfigService,
+  ) {}
 
   async uploadToSupabase(file: Express.Multer.File): Promise<any> {
     try {
-      const bucketName = process.env.SUPABASE_BUCKET_NAME;
+      const bucketName = this.configService.get<string>('post_SUPABASE_BUCKET');
       const filePath = `${Date.now()}-${file.originalname}`;
 
       const { data, error } = await this.supabaseService.supabase.storage
